1d4c1fb328e1a5d369cf87384fe66cf97dd99274
Naseem Ansari <mrnaseem745@gmail.com>
2024-08-11 13:36:42 +0530

    #responsive for all devices

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,8 @@ import { amazonLogo, shoeImg } from '../assets'
 
 const Hero = () => {
   return (
-    <section className='grid grid-cols-2 items-center max-lg:grid-cols-1 gap-4 font-poppins pl-20 py-10'>
-        <div className="shoe-text flex-center">
+    <section className='grid grid-cols-2 items-center max-lg:grid-cols-1 gap-4 font-poppins pl-20 py-10 max-lg:pl-0'>
+        <div className="shoe-text flex-center max-md:order-last">
             <h1 className='hero-title'>
             NIKE EVO <br />
             GEN Z V2 <br />
@@ -17,10 +17,12 @@ const Hero = () => {
                 <button className='btn'>Shop Now</button>
                 <button className='btn-var btn-hover'>Varients</button>
             </div>
+            <div className="logo-pos">
             <p className='hero-para'>Also Available On</p>
-            <img src={amazonLogo} className='logo-pos' />
+            <img src={amazonLogo} className='' />
+            </div>
         </div>
-        <div className="shoe-img w-[500px]">
+        <div className="shoe-img w-[500px] max-md:w-fit">
             <img src={shoeImg} />
         </div>
     </section>
